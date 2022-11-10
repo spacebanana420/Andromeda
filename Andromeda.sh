23f9b148b89cc61a153f4e4ca37c48275d7fcf31
Space Banana <98528301+spacebanana420@users.noreply.github.com>
2022-11-10 13:37:26 +0000

    Add files via upload

---
+++ Andromeda.sh
@@ -0,0 +1,96 @@
+#!/bin/bash
+#Requirements: Bash, Lua, coreutils, zip, unzip
+
+echo "Andromeda Password Manager (version 0.4 dev build 2)"; echo ""
+if [[ -e config.txt ]]
+then
+    if [[ $(cat config.txt) != *"version=0.4"* ]]
+    then
+        echo "Warning: config file might not be supported by this version"
+    fi
+else
+    echo "version=0.3" > config.txt; echo "file=zip" >> config.txt; echo "password=dictionary" >> config.txt; echo "ascii_char_length=60" >> config.txt; echo "dictionary_char_length=60" >> config.txt; echo "default_dictionary=true" >> config.txt; echo "dictionary_separator=space" >> config.txt;
+fi
+
+#Config read
+if [[ $(cat config.txt) == *"password=ascii"* ]]
+then
+    luapass=1
+else
+    luapass=2
+fi
+if [[ $(cat config.txt) == *"default_dictionary=true"* ]]
+then
+    defdictionary=dictionary.txt
+fi
+#End of config read
+
+databases(){ #Choose, create and edit databases
+if [[ -e databases.txt  && -e *$(cat databases.txt)* ]]
+then
+    ls *$(cat databases.txt)*; echo ""
+    echo "Choose a database or create a new one"
+    read database
+    echo "Input database password"
+    read datapass
+    if [[ $database == *$(cat databases.txt)* ]]
+    then
+        unzip -P "$datapass" $database.zip
+    else
+        echo $database >> databases.txt
+        mkdir "$database"
+    fi
+else
+    echo "No databases found. Give a name for the new database or leave blank for 'database0'"
+    read database
+    if [[ $database == "" ]]
+    then
+        database="database0"
+    fi
+    echo "Give a password for the database or leave blank for autogeneration"
+    read datapass
+    if [[ $datapass == "" ]]
+    then
+        datapass=$(lua passgen.lua $luapass 60 $defdictionary)
+        echo "A password has been generated for your database:"; echo "$datapass"
+    fi
+    echo $database > databases.txt
+    mkdir $database
+fi
+mv "$database" ".$database"
+cd ".$database"
+readdatabase
+}
+
+readdatabase() { #Manage the stored passwords in the database
+ls; echo ""; echo "Choose an action"; echo "add  remove  view"
+read action
+case $action in
+add)
+    echo "Give a name for this password entry"
+    read entry
+    if [[ -e $entry ]]
+    then
+        echo "Entry already exists, the password will be changed"
+    fi
+    echo "Input password"
+    read password; echo $password > $entry
+;;
+remove)
+    echo "Choose an entry to remove"
+    read entry
+;;
+view)
+    for i in *
+    do
+        echo $i ": " $(cat $i)
+    done
+;;
+esac
+cd ..
+mv ".$database" "$database"
+zip -3 -r -q -P "$datapass" "$database.zip" "$database" #Database encryption uses AES on a zip archive for flexibility and global support
+rm -r "$database"
+databases
+}
+databases
---
+++ passgen.lua
@@ -0,0 +1,86 @@
+choice = {
+    ["1"] = function() --ASCII password: arg 2 is character length
+        chars={}
+        password = ""
+        charlength = math.floor(tonumber(arg[2]))
+
+        for i = 0, charlength-1 do
+            chars[i] = string.char(math.random(33, 126))
+            password = password .. chars[i]
+        end
+        print(password)
+    end,
+    ["2"] = function() --Dictionary password: arg 2 is keyword length, arg 3 is dictionary text file
+        keywords={}
+        randompick={}
+        password = ""
+        wordlength = math.floor(tonumber(arg[2]))
+        dictionary = io.open("dictionary.txt", "r")
+        io.input(dictionary)
+        a=0
+        b=true
+        while (b == true)
+        do
+            lineread = io.read("l")
+            if (lineread ~= nil)
+            then
+                keywords[a] = lineread
+                a = a+1
+            else
+                b=false
+            end
+        end
+        --[[for i = 0, wordlength - 1 do -- Add the keyword list from the dictionary
+            if (io.read("l") ~= "nil")
+            then
+                keywords[i] = io.read("l")
+            else
+                io.read("l")
+                wordlength=i
+            end
+        end]]--
+        --if (a < wordlength-1)
+        --then
+        --    wordlength=a
+        --end
+        for i = 0, wordlength - 1 do --Randomize the words to create the password
+            randompick[i] = keywords[math.random(0, a-1)]
+            while (randompick[i] == randompick[i-1]) do -- Equal Keywords cannot be side by side
+                if (randompick[i] == randompick[i-1])
+                then
+                    randompick[i] = keywords[math.random(0, a-1)]
+                end
+            end
+            password = password .. randompick[i] .. " "
+        end
+        print(password)
+    end
+}
+if (choice[arg[1]])
+then
+    choice[arg[1]]()
+end
+--[[
+chars={}
+password = ""
+print("Character length?")
+charlength = io.read()
+charlength = math.floor(tonumber(charlength))
+
+for i = 0, charlength-1 do
+    chars[i] = string.char(math.random(33, 126))
+    password = password .. chars[i]
+end
+print(password)
+if (charlength < 155)
+then
+    possiblepasswords = math.pow(93, charlength)
+    print("")
+    print("Possible passwords: " .. possiblepasswords)
+    entropy = math.log(possiblepasswords, 2)
+    print("Entropy: " .. entropy .. " bit")
+else
+    print("")
+    print("Entropy: >1000 bit")
+end
+--]]
