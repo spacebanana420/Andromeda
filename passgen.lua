6e766ef9f51cf5e61786a575ea9a9be6cf1e9f25
Space Banana <98528301+spacebanana420@users.noreply.github.com>
2022-11-25 21:17:55 +0000

    Update passgen.lua

--- passgen.lua
+++ passgen.lua
@@ -29,19 +29,6 @@ choice = {
                 b=false
             end
         end
-        --[[for i = 0, wordlength - 1 do -- Add the keyword list from the dictionary
-            if (io.read("l") ~= "nil")
-            then
-                keywords[i] = io.read("l")
-            else
-                io.read("l")
-                wordlength=i
-            end
-        end]]--
-        --if (a < wordlength-1)
-        --then
-        --    wordlength=a
-        --end
         for i = 0, wordlength - 1 do --Randomize the words to create the password
             randompick[i] = keywords[math.random(0, a-1)]
             while (randompick[i] == randompick[i-1]) do -- Equal Keywords cannot be side by side
